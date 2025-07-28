30fd99fd189c0d0c3a3c9e96e733fc7b3cc25ce2
Mick <wang.mick12@gmail.com>
2025-07-28 16:27:35 -0400

    add authentication screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,8 @@ import './global.css';
 
 export default function RootLayout() {
   return <Stack>
+    <Stack.Screen name="index" options={{ headerShown: false }} />
     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     <Stack.Screen name="exercises/[id]" options={{ title: 'Exercise Details' }} />
-    <Stack.Screen name="(tabs)/exercise" options={{ title: 'Exercises' }} />
-    <Stack.Screen name="(tabs)/planner" options={{ title: 'Planner' }} />
   </Stack>
 }
