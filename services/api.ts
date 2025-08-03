19ce7c2f732a0f458f4ea64ba80ef5374d2e2100
Mick <wang.mick12@gmail.com>
2025-08-03 13:47:46 -0400

    create custom hook to fetch exercises

--- services/api.ts
+++ services/api.ts
@@ -1,23 +1,25 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, doc, getDoc, query } from "firebase/firestore";
 import { Routine, Exercise } from "@/.expo/types/routine";
 import { db } from "../FirebaseConfig";
 
 export const fetchRoutines = async (): Promise<Routine[]> => {
   const routinesQuery = query(collection(db, "routines"));
-  const querySnapshot = await getDocs(routinesQuery);
+  const querySnap = await getDocs(routinesQuery);
 
-  return querySnapshot.docs.map((doc) => ({
+  return querySnap.docs.map((doc) => ({
     id: doc.id,
     ...(doc.data() as Omit<Routine, "id">),
   }));
 };
 
-export const fetchExercise = async (exerciseId: string): Promise<Exercise[]> => {
-  const exercisesQuery = query(collection(db, "exercises"), where("exerciseId", "==", exerciseId));
-  const querySnapshot = await getDocs(exercisesQuery);
+export const fetchExercise = async (exerciseId: string): Promise<Exercise> => {
+  const docRef = doc(db, "exercises", exerciseId);
+  const docSnap = await getDoc(docRef);
 
-  return querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...(doc.data() as Omit<Exercise, "id">),
-  }));
+  if (!docSnap.exists()) {
+    throw new Error(`Exercise with ID ${exerciseId} not found`);
+  }
+
+  return docSnap.data() as Omit<Exercise, "id">;
+
 };
