660270adab3ffc90c7f31d7eb009c035505046fb
Mick <wang.mick12@gmail.com>
2026-08-04 22:48:34 -0400

    render real data instead of stub

--- app/exercises/[id].tsx
+++ app/exercises/[id].tsx
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import { fetchExercise } from "@/services/api";
+import { useFetch } from "@/services/useFetch";
+import ExerciseItem from "@/components/ExerciseItem";
 
 const ExerciseDetails = () => {
-  const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams<{ id: string }>();
+  const fetchFn = React.useCallback(() => fetchExercise(id), [id]);
+  const { data: exercise, loading, error } = useFetch(fetchFn);
+
+  if (loading) return <ActivityIndicator size="large" />;
+  if (error || !exercise) return <Text>Exercise not found</Text>;
+
   return (
     <View>
-      <Text>ExerciseDetails</Text>
+      <ExerciseItem exercise={exercise} />
     </View>
   );
 };
